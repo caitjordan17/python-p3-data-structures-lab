cb2ece503651006847f6ed096fdf62e9b87a554f
Caitlin Jordan <jordan.cait17@gmail.com>
2023-11-23 08:55:29 -0800

    completed lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,42 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return [item["name"] for item in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    heat_array = []
+    for item in spicy_foods:
+        if item["heat_level"] > 5:
+            heat_array.append(item)
+    
+    return heat_array
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {"🌶" * food["heat_level"]}')
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {"🌶" * food["heat_level"]}')
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat = []
+    for food in spicy_foods:
+        heat.append(food["heat_level"])
+    return sum(heat) / len(heat)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
+
+    # max_heat = max([item["heat_level"] for item in spicy_foods])
+    # for item in spicy_foods:
+    #     if item["heat_level"] == max_heat:
+    #         return item
